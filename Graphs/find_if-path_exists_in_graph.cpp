20e903ac0021b39210df004bc45510b184e4be1a
Mohammed Saqib <57943299+sakibchonka@users.noreply.github.com>
2025-01-05 05:22:42 +0530

    1971. Find if Path Exists in Graph

---
+++ Graphs/find_if-path_exists_in_graph.cpp
@@ -0,0 +1,24 @@
+// 1971. Find if Path Exists in Graph
+
+void dfs(int node, vector<vector<int>>& adj, vector<bool> &visited) {
+    visited[node] = true;
+
+    for(int neighbor : adj[node]) {
+        if(!visited[neighbor]) {
+            dfs(neighbor, adj, visited);
+        }
+    }
+}
+
+bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    
+    vector<bool> visited(n, false);
+    vector<vector<int>> adj(n);
+    for(const auto &edge : edges) {
+        adj[edge[0]].push_back(edge[1]);
+        adj[edge[1]].push_back(edge[0]);
+    }
+    dfs(source, adj, visited);
+
+    return visited[destination];
+}
